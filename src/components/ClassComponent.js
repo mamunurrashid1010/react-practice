059191c45a181bfce04b9cd399fd1569b2d29fd1
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-12-10 12:02:15 +0600

    Component : Create class component and add example

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class ClassComponent extends Component{
+    render(){
+        return(
+            <div>
+                <h5>I'm from Class component.</h5>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centurie
+                </p>
+            </div>
+        );
+    }
+}
+export default ClassComponent;
